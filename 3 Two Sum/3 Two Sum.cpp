72f9c5d52544e67a4505333e0bc5a966a013e9da
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-12-12 02:41:12 -0600

    Update 3 Two Sum.cpp

--- 3 Two Sum/3 Two Sum.cpp
+++ 3 Two Sum/3 Two Sum.cpp
@@ -9,7 +9,6 @@ struct TestCase {
     vector<int> nums;      // Input numbers
     int target;            // Target Sum
     vector<int> expected;  // expected output
-    string explaination;   // Explanation on why.
 };
 
 vector<int> twoSum(vector<int>& nums, int target);
@@ -17,9 +16,9 @@ vector<int> twoSum(vector<int>& nums, int target);
 int main()
 {
     vector<TestCase> TestCases = {
-        {{3,4,5,6}, 7, {0,1}, "Explanation: nums[0] + nums[1] == 7, so we return {0, 1}." },
-        {{4,5,6}, 10, {0,2}, "Explanation: nums[0] + nums[2] == 10, so we return {0, 2}." },
-        {{5,5}, 10, {0,1}, "Explanation: nums[0] + nums[1] == 10, so we return {0, 1}." },
+        {{3,4,5,6}, 7, {0,1}},
+        {{4,5,6}, 10, {0,2}},
+        {{5,5}, 10, {0,1}}
     }
 
     for (case: TestCases) {
