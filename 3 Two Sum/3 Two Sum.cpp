0cb310527e9d0a0cc3feb00d3c399b3c55b909fd
King Deyo <36089323+TrevorDeyo@users.noreply.github.com>
2024-12-12 02:22:57 -0600

    Update 3 Two Sum.cpp

--- 3 Two Sum/3 Two Sum.cpp
+++ 3 Two Sum/3 Two Sum.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <pair>
+#include <utility>
+#include <string>
 
 using namespace std;
 
-void twoSum() {
-    
-}
+struct TestCase {
+    vector<int> nums;      // Input numbers
+    int target;            // Target Sum
+    vector<int> expected;  // expected output
+    string explaination;   // Explanation on why.
+};
+
+vector<int> twoSum(vector<int>& nums, int target);
 
 int main()
 {
-    pair<vector<int>, int> nums = {3, 4, 5, 6}
+    vector<TestCase> TestCases = {
+        {{3,4,5,6}, 7, {0,1}, "Explanation: nums[0] + nums[1] == 7, so we return [0, 1]." },
+        {{4,5,6}, 7, {0,1}
+    { {3,4,5,6}, 7 }
+    { {3,4,5,6}, 7 }
 
     for (index )
     twoSum();
 
     cout << "Hello World!\n";
     return 0;
+}
+
+// PLACEHOLDER
+vector<int> twoSum(vector<int>& nums, int target) {
+    return {};
 }
